c23b336fca5a8b33caccefe5b817500808c09d80
MGrigoryan <mgrigor868@student.glendale.edu>
2017-12-01 22:29:02 -0800

    Corrected modes and didnt know what to do with xBee stuff for Thing

--- delta_MC_Controller/set_new_speed.ino
+++ delta_MC_Controller/set_new_speed.ino
@@ -6,6 +6,8 @@
  *   Output:
  *    1)Current Speed
  *  
+ *  
+ *  check if getting xbee is on, if not then automatically slow mode
  */
 
  
@@ -37,8 +39,10 @@ if(eStop) {
 
   else {
 
- //************************Rover Mode*******************************
+ //************************Rover Mode with joystick *******************************
+   if(xBee = 0){
     
+    if (mode == MODE_ROVER){
      if( !( (goal_spd_lt-goal_thr < cur_spd_lt) && 
            (goal_spd_lt+goal_thr > cur_spd_lt) ) ) {  
 
@@ -60,24 +64,25 @@ if(eStop) {
                 cur_spd_rt -= goal_ramp;
            }      
          }
-      
-   }  
+      }
+     
 
 
-// ********************* Arm Mode **********************
-  if (robotID = 1)
+// ********************* Arm Mode with joystick**********************
+  else if (mode == MODE_ARM)
   {       
-  
+ 
   if ( ! ( (goal_spd_m1-goal_thr < cur_spd_m1)  && 
             (goal_spd_m1+goal_thr > cur_spd_m1) )  )       // if speed is not within range 
-     {  
-    if (cur_spd_m1 < goal_spd_m1)
-      {
-      cur_spd_m1 += goal_ramp;      // increase speed
-       }
-     else {  
-     cur_spd_m1 -= goal_ramp;     //decrease speed
-      }
+    {  
+          if (cur_spd_m1 < goal_spd_m1)
+          {
+             cur_spd_m1 += goal_ramp;      // increase speed
+           }
+              else 
+              {  
+                cur_spd_m1 -= goal_ramp;     //decrease speed
+              }
      }
     if ( ! ( (goal_spd_m2-goal_thr < cur_spd_m2)  && 
         (goal_spd_m2 + goal_thr > cur_spd_m2) ) )       // Motor 2
@@ -86,61 +91,177 @@ if(eStop) {
         {
             cur_spd_m2 += goal_ramp;
         }
-    else {  
-     cur_spd_m2 -= goal_ramp;
-    }
-   }
+          else
+          {  
+            cur_spd_m2 -= goal_ramp;
+          }
+      }
+    
     if ( ! ( (goal_spd_m3-goal_thr < cur_spd_m3)  && 
       (goal_spd_m3 + goal_thr > cur_spd_m3) ) )       // Motor 3
      {  
-     if (cur_spd_m3 < goal_spd_m3)
-    {
-      cur_spd_m3 += goal_ramp;
-    }
-     else {  
-     cur_spd_m3 -= goal_ramp;
+           if (cur_spd_m3 < goal_spd_m3)
+           {
+            cur_spd_m3 += goal_ramp;
+            }
+           else 
+           {  
+            cur_spd_m3 -= goal_ramp;
+            }
       }
-    }
-  
+
     if ( ! ( (goal_spd_m4 - goal_thr < cur_spd_m4)  && 
       (goal_spd_m4 + goal_thr > cur_spd_m4) ) )       // Motor 4 
      {  
-    if (cur_spd_m4 < goal_spd_m4)
-      {
-      cur_spd_m4 += goal_ramp;
-      }
-else {  
-     cur_spd_m4 -= goal_ramp;
-}
-   }
+          if (cur_spd_m4 < goal_spd_m4)
+          {
+           cur_spd_m4 += goal_ramp;
+          }
+          else
+          {  
+           cur_spd_m4 -= goal_ramp;
+          }
+     }
 
-if ( ! ( (goal_spd_m5 - goal_thr < cur_spd_m5)  && 
+    if ( ! ( (goal_spd_m5 - goal_thr < cur_spd_m5)  && 
         (goal_spd_m5 + goal_thr > cur_spd_m5) ) )       // Motor 5 
      {  
-    if (cur_spd_m5 < goal_spd_m5)
- {
-      cur_spd_m5 += goal_ramp;
- }
-else {  
-     cur_spd_m5 -= goal_ramp;
-}
-   }
+         if (cur_spd_m5 < goal_spd_m5)
+          {
+            cur_spd_m5 += goal_ramp;
+          }
+          else {  
+            cur_spd_m5 -= goal_ramp;
+           }
+      }
 
 
-if ( ! ( (goal_spd_m6 - goal_thr < cur_spd_m6)  && 
+    if ( ! ( (goal_spd_m6 - goal_thr < cur_spd_m6)  && 
         (goal_spd_m6 + goal_thr > cur_spd m6) ) )       // Motor 6 
-     {  
-    if (cur_spd_m6 < goal_spd_m6)
- {
-      cur_spd_m6 += goal_ramp;
- }
-else {  
-     cur_spd_m6 -= goal_ramp;
+    {  
+       if (cur_spd_m6 < goal_spd_m6)
+        {
+          cur_spd_m6 += goal_ramp;
+        }
+        else {  
+         cur_spd_m6 -= goal_ramp;
+         }
+      }
     }
    }
-
-}
-//************************End of Arm Mode************************ 
-
+//************************End of Arm Mode*****************************
+ 
+  else if(xBee = 1)     // if thing is not alive and THING comes in
+  { 
+   drive_mode = SLOW;   // drive mode for arm and rover should be slow 
   
-} //DONE
+//************************Rover Mode with THING************************  
+     if (mode == MODE_ROVER){
+     if( !( (goal_spd_lt-goal_thr < cur_spd_lt) && 
+           (goal_spd_lt+goal_thr > cur_spd_lt) ) ) {  
+
+        if(cur_spd_lt < goal_spd_lt) {
+           cur_spd_lt += goal_ramp;
+           }
+        else {
+          cur_spd_lt -= goal_ramp;
+          }
+     }
+
+      if( !( (goal_spd_rt-goal_thr < cur_spd_rt) && 
+           (goal_spd_rt+goal_thr > cur_spd_rt) ) ) { 
+
+    if(cur_spd_rt < goal_spd_rt) {
+              cur_spd_rt += goal_ramp;
+           }
+           else {
+                cur_spd_rt -= goal_ramp;
+           }      
+         }
+      }
+     
+
+
+// ********************* Arm Mode with THING**********************
+  else if (mode == MODE_ARM)
+  {       
+ 
+  if ( ! ( (goal_spd_m1-goal_thr < cur_spd_m1)  && 
+            (goal_spd_m1+goal_thr > cur_spd_m1) )  )       // if speed is not within range 
+    {  
+          if (cur_spd_m1 < goal_spd_m1)
+          {
+             cur_spd_m1 += goal_ramp;      // increase speed
+           }
+              else 
+              {  
+                cur_spd_m1 -= goal_ramp;     //decrease speed
+              }
+     }
+    if ( ! ( (goal_spd_m2-goal_thr < cur_spd_m2)  && 
+        (goal_spd_m2 + goal_thr > cur_spd_m2) ) )       // Motor 2
+     {  
+        if (cur_spd_m2 < goal_spd_m2)
+        {
+            cur_spd_m2 += goal_ramp;
+        }
+          else
+          {  
+            cur_spd_m2 -= goal_ramp;
+          }
+      }
+    
+    if ( ! ( (goal_spd_m3-goal_thr < cur_spd_m3)  && 
+      (goal_spd_m3 + goal_thr > cur_spd_m3) ) )       // Motor 3
+     {  
+           if (cur_spd_m3 < goal_spd_m3)
+           {
+            cur_spd_m3 += goal_ramp;
+            }
+           else 
+           {  
+            cur_spd_m3 -= goal_ramp;
+            }
+      }
+
+    if ( ! ( (goal_spd_m4 - goal_thr < cur_spd_m4)  && 
+      (goal_spd_m4 + goal_thr > cur_spd_m4) ) )       // Motor 4 
+     {  
+          if (cur_spd_m4 < goal_spd_m4)
+          {
+           cur_spd_m4 += goal_ramp;
+          }
+          else
+          {  
+           cur_spd_m4 -= goal_ramp;
+          }
+     }
+
+    if ( ! ( (goal_spd_m5 - goal_thr < cur_spd_m5)  && 
+        (goal_spd_m5 + goal_thr > cur_spd_m5) ) )       // Motor 5 
+     {  
+         if (cur_spd_m5 < goal_spd_m5)
+          {
+            cur_spd_m5 += goal_ramp;
+          }
+          else {  
+            cur_spd_m5 -= goal_ramp;
+           }
+      }
+
+
+    if ( ! ( (goal_spd_m6 - goal_thr < cur_spd_m6)  && 
+        (goal_spd_m6 + goal_thr > cur_spd m6) ) )       // Motor 6 
+    {  
+       if (cur_spd_m6 < goal_spd_m6)
+        {
+          cur_spd_m6 += goal_ramp;
+        }
+        else {  
+         cur_spd_m6 -= goal_ramp;
+         }
+      }
+    } 
+  }
+// ********************* END OF ARM MODE **********************
+} //DONE...for now 
