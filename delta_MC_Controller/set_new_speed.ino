79a35ca1faaed609d162e1d3d499037f7b9874d6
Avery <averyalexanderball@gmail.com>
2017-12-01 17:59:59 -0800

    Changes
    added arm_spd_ur = 0 values for 1-6 in arm
    altered comments

--- delta_MC_Controller/set_new_speed.ino
+++ delta_MC_Controller/set_new_speed.ino
@@ -1,44 +1,146 @@
-// given the current commands, set the drive speed value
+/*  
+ *   First see what kind of input it is:
+ *          1) Arm    robotID = 1
+ *          2) Rover  robotID = 0
+ *   if arm do "this" code else if rover do "this" code
+ *   
+ *   dangerOverride = 1 means ignore the "danger"
+ *                  = 0 do danger code (see getDanger)
+ */
 
-void set_new_speed() {
+ 
+void set_speed()
+{
+  int goal_thr = param[vel_mode].thr;
+  int goal_ramp = param[vel_mode].ramp;
 
-  int goal_thr = param[drive_mode].thr;
-  int goal_ramp = param[drive_mode].ramp;
-
-  if(eStop) { 
+if(eStop) { 
     goal_spd_lt = 0;
     goal_spd_rt = 0;
     cur_spd_lt = 0;
     cur_spd_rt = 0;
+    
+    goal_spd_m1 = 0;
+    goal_spd_m2 = 0;
+    goal_spd_m3 = 0;
+    goal_spd_m4 = 0; 
+    goal_spd_m5 = 0;
+    goal_spd_m6 = 0;
+
+    cur_spd_m1 = 0;
+    cur_spd_m2 = 0;
+    cur_spd_m3 = 0;
+    cur_spd_m4 = 0;
+    cur_spd_m5 = 0;
+    cur_spd_m6 = 0;
   }
 
-  else {  // system OK, figure out next speed
+  else {
 
-    // figure out left side
-    if( !( (goal_spd_lt-goal_thr < cur_spd_lt) && 
-           (goal_spd_lt+goal_thr > cur_spd_lt) ) ) {  // if NOT within RANGE
-      // are we increasing or decreasing?
-      if(cur_spd_lt < goal_spd_lt) {
-        cur_spd_lt += goal_ramp;
-      }
-      else {
-        cur_spd_lt -= goal_ramp;
-      }
-    }
+ //************************Rover Mode*******************************
+    
+     if( !( (goal_spd_lt-goal_thr < cur_spd_lt) && 
+           (goal_spd_lt+goal_thr > cur_spd_lt) ) ) {  
 
-    // figure out right side
-    if( !( (goal_spd_rt-goal_thr < cur_spd_rt) && 
-           (goal_spd_rt+goal_thr > cur_spd_rt) ) ) {  // if NOT within RANGE
-      // are we increasing or decreasing?
-      if(cur_spd_rt < goal_spd_rt) {
-        cur_spd_rt += goal_ramp;
-      }
-      else {
-        cur_spd_rt -= goal_ramp;
-      }      
-    }
+        if(cur_spd_lt < goal_spd_lt) {
+           cur_spd_lt += goal_ramp;
+           }
+        else {
+          cur_spd_lt -= goal_ramp;
+          }
+     }
 
-  } // else figure out next speed
+      if( !( (goal_spd_rt-goal_thr < cur_spd_rt) && 
+           (goal_spd_rt+goal_thr > cur_spd_rt) ) ) { 
+
+    if(cur_spd_rt < goal_spd_rt) {
+              cur_spd_rt += goal_ramp;
+           }
+           else {
+                cur_spd_rt -= goal_ramp;
+           }      
+         }
+      
+   }  
+
+
+// ********************* Arm Mode **********************
+  if (robotID = 1)
+  {       
   
-} // set_new_speed()
+  if ( ! ( (goal_spd_m1-goal_thr < cur_spd_m1)  && 
+            (goal_spd_m1+goal_thr > cur_spd_m1) )  )       // if speed is not within range 
+     {  
+    if (cur_spd_m1 < goal_spd_m1)
+      {
+      cur_spd_m1 += goal_ramp;      // increase speed
+       }
+     else {  
+     cur_spd_m1 -= goal_ramp;     //decrease speed
+      }
+     }
+    if ( ! ( (goal_spd_m2-goal_thr < cur_spd_m2)  && 
+        (goal_spd_m2 + goal_thr > cur_spd_m2) ) )       // Motor 2
+     {  
+        if (cur_spd_m2 < goal_spd_m2)
+        {
+            cur_spd_m2 += goal_ramp;
+        }
+    else {  
+     cur_spd_m2 -= goal_ramp;
+    }
+   }
+    if ( ! ( (goal_spd_m3-goal_thr < cur_spd_m3)  && 
+      (goal_spd_m3 + goal_thr > cur_spd_m3) ) )       // Motor 3
+     {  
+     if (cur_spd_m3 < goal_spd_m3)
+    {
+      cur_spd_m3 += goal_ramp;
+    }
+     else {  
+     cur_spd_m3 -= goal_ramp;
+      }
+    }
+  
+    if ( ! ( (goal_spd_m4 - goal_thr < cur_spd_m4)  && 
+      (goal_spd_m4 + goal_thr > cur_spd_m4) ) )       // Motor 4 
+     {  
+    if (cur_spd_m4 < goal_spd_m4)
+      {
+      cur_spd_m4 += goal_ramp;
+      }
+else {  
+     cur_spd_m4 -= goal_ramp;
+}
+   }
 
+if ( ! ( (goal_spd_m5 - goal_thr < cur_spd_m5)  && 
+        (goal_spd_m5 + goal_thr > cur_spd_m5) ) )       // Motor 5 
+     {  
+    if (cur_spd_m5 < goal_spd_m5)
+ {
+      cur_spd_m5 += goal_ramp;
+ }
+else {  
+     cur_spd_m5 -= goal_ramp;
+}
+   }
+
+
+if ( ! ( (goal_spd_m6 - goal_thr < cur_spd_m6)  && 
+        (goal_spd_m6 + goal_thr > cur_spd m6) ) )       // Motor 6 
+     {  
+    if (cur_spd_m6 < goal_spd_m6)
+ {
+      cur_spd_m6 += goal_ramp;
+ }
+else {  
+     cur_spd_m6 -= goal_ramp;
+    }
+   }
+
+}
+//************************End of Arm Mode************************ 
+
+  
+} //DONE
