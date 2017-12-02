66cdbaa56f68f4ed1841035f21de7d090f606707
AndrewN98 <andnazarians@gmail.com>
2017-12-01 21:04:05 -0800

    another update

--- delta_MC_Controller/getDanger.ino
+++ delta_MC_Controller/getDanger.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Globals
 //***********************
 bool dangerFront = false;
@@ -19,28 +18,6 @@ bool dangerM3 = false;
 bool dangerM4 = false;
 bool dangerM5 = false;
 bool dangerM6 = false;   
-=======
-
-//Globals
-//***********************
-bool dangerFront = false;    
-bool dangerBack = false;
-
-int currentM1 = 0;
-int currentM2 = 0;
-int currentM3 = 0;
-int currentM4 = 0;
-int currentM5 = 0;
-int currentM6 = 0;
-
-
-bool dangerMotor1 = false;
-bool dangerMotor2 = false;
-bool dangerMotor3 = false;
-bool dangerMotor4 = false;
-bool dangerMotor5 = false;
-bool dangerMotor6 = false;   
->>>>>>> eb9166e364bc91493cbb9364c7658d9073abf511
 
 bool FrontRight = false;
 bool FrontLeft = false;
@@ -48,21 +25,12 @@ bool BackRight = false;
 bool BackLeft = false;
 //***********************
 
-<<<<<<< HEAD
 int M1_thresh = 1;
 int M2_thresh = 2;
 int M3_thresh = 3;
 int M4_thresh = 4;
 int M5_thresh = 5;
 int M6_thresh = 6;              // undertermined values; requires testing
-=======
-int M1_thresh;
-int M2_thresh;
-int M3_thresh;
-int M4_thresh;
-int M5_thresh;
-int M6_thresh;                  // undertermined values; requires testing
->>>>>>> eb9166e364bc91493cbb9364c7658d9073abf511
 
 int BUTTON_PIN_FRONT_R = 43;
 int BUTTON_PIN_FRONT_L = 41;
@@ -82,7 +50,6 @@ void setup()
   pinMode(BUTTON_PIN_BACK_L, INPUT);
 }
 
-<<<<<<< HEAD
 void loop() {
 
   if( dangerOverride = true){
@@ -101,18 +68,11 @@ void loop() {
 
   if( mode = MODE_ROVER){
 
-=======
-void loop() 
-{
-  if( mode = 0)
-      {
->>>>>>> eb9166e364bc91493cbb9364c7658d9073abf511
         buttonStateFront_R = digitalRead(BUTTON_PIN_FRONT_R);
         buttonStateBack_R = digitalRead(BUTTON_PIN_BACK_R);
         buttonStateFront_L = digitalRead(BUTTON_PIN_FRONT_L);
         buttonStateBack_L = digitalRead(BUTTON_PIN_BACK_L);         
 
-<<<<<<< HEAD
         if (buttonStateFront_R || buttonStateFront_L == HIGH){
           dangerFront = true;
         }
@@ -166,31 +126,3 @@ void loop()
     }
   }
 }
-=======
-        if (buttonStateFront_R || buttonStateFront_L == HIGH)
-        {
-            dangerFront = true;
-        }
-        else 
-        {
-            dangerFront = false;
-        }
-  
-        if (buttonStateBack_R || buttonStateBack_L == HIGH)
-        {
-            dangerBack = true;
-        }
-        else
-        {
-            dangerBack = false;
-        }
-      }
-
-  if( mode = 1) //arm
-    {
-      if (
-    
-    }
-  
-}
->>>>>>> eb9166e364bc91493cbb9364c7658d9073abf511
